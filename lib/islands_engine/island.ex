2a0e04a4c6153292d7afc77c96d3e3905554efe0
Domício Medeiros <domicioam@gmail.com>
2023-01-07 20:30:45 +0000

    all functionality for island

--- lib/islands_engine/island.ex
+++ lib/islands_engine/island.ex
@@ -14,11 +14,28 @@ defmodule IslandsEngine.Island do
     end
   end
 
+  def overlaps?(existing_island, new_island), do:
+    not MapSet.disjoint?(existing_island.coordinates, new_island.coordinates)
+
   defp add_coordinates(offsets, upper_left) do
     Enum.reduce_while(offsets, MapSet.new(), fn offset, acc ->
       add_coordinate(acc, upper_left, offset)
     end)
   end
+
+  def guess(island, coordinate) do
+    case MapSet.member?(island.coordinates, coordinate) do
+      true ->
+        hit_coordinates = MapSet.put(island.hit_coordinates, coordinate)
+        {:hit, %{island | hit_coordinates: hit_coordinates}}
+      false -> :miss
+    end
+  end
+
+  def forested?(island), do:
+    MapSet.equal?(island.coordinates, island.hit_coordinates)
+
+  def types(), do: [:atoll, :dot, :l_shape, :s_shape, :square]
 
   defp add_coordinate(coordinates, %Coordinate{row: row, col: col},
     {row_offset, col_offset}) do
